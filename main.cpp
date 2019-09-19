23b92a39120f142a9c3209961c17a98eee39382e
evf <eliseuv816@gmail.com>
2019-09-19 14:17:09 -0300

    Program complete but has memory issues

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <cmath>
 #include <vector>
 #include <random>
@@ -30,13 +31,17 @@ double giniIndex(const std::vector<double>& vec) {
 
 int main()
 {
+    // Output file
+    std::ofstream   data_file1("data1.dat", std::ios::out),
+                    data_file2("data2.dat", std::ios::out);
+
     // System dimensions
     const int l_x = 128;
     const int l_y = 1;
 
     const int it_max = 1;
     const int idelta_max = 1;
-    const int idt = 1000;
+    const int i_dt = 1000;
     const double epsilon = 1e-4;
     const double erro = 1e-1;
     const double dif = 0;
@@ -54,7 +59,7 @@ int main()
     double rho_total = static_cast<double>(n_total)/(l_x*l_y);
 
     //
-    const int n_run = 1000/n_total + 200;
+    int n_run = 1000/n_total + 200;
     int rand_seed = 793;
     std::mt19937 engine(rand_seed);
     std::uniform_real_distribution<double> uniform_dist(0.0, 1.0);
@@ -96,12 +101,15 @@ int main()
         // Particle positions
         std::vector<int> in_x(n_total), in_y(n_total);
 
-        // Measured parameters
-        double  avg_mobility = 0, var_mobility = 0,                             // Mobility
-                avg_phi_x = 0, avg_phi_y = 0, var_phi_x = 0, var_phi_y = 0,     // Order parameter
-                avg_j_a = 0, avg_j_b = 0, var_j_a = 0, var_j_b = 0,             // Current density
-                avg_gini_a = 0, avg_gini_b = 0, var_gini_a = 0, var_gini_b = 0, // Gini index
-                p_lane = 0, p_clog = 0, p_else = 0;                             // Probabilities assign to each scenario
+        // Measured parameters (sample statistics)
+        double  savg_mobility = 0, svar_mobility = 0,                               // Mobility
+                savg_phi_x = 0, savg_phi_y = 0, svar_phi_x = 0, svar_phi_y = 0,     // Order parameter
+                savg_j_a = 0, savg_j_b = 0, savg_j = 0, // Current density
+                svar_j_a = 0, svar_j_b = 0, svar_j = 0,
+                savg_gini_a = 0, savg_gini_b = 0, svar_gini_a = 0, svar_gini_b = 0; // Gini index
+
+        // Probabilities assign to each scenario
+        double p_lane = 0, p_clog = 0, p_else = 0;
 
         // Sample loop
         for (int i_run = 0; i_run < n_run; i_run++) {
@@ -130,7 +138,10 @@ int main()
             // Aux variables
             int mc_iter = 0;                            // Monte Carlo iteration
             int i_count = 1, i_count2 = 0;              // Stopping criterium interval index and time step
-            int ism=0, isx=0, isy=0, isga=0, islo=0;    // Aux vars
+            bool    is_mobility_stat = false,
+                    is_phi_x_stat = false,
+                    is_phi_y_stat = false,
+                    is_gini_a_stat = false;
             double  a=0, b=0, c=0, d=0;                 // Aux vars
             double  d_mom_mobility = 0,                 // Time average of mobility over interval i_dt
                     d_mom_phi_x = 0, d_mom_phi_y = 0,   // Time average of order parameter over interval i_dt
@@ -138,7 +149,7 @@ int main()
                     d_mom_gini_a = 0, d_mom_gini_b = 0; // Time average of Gini index over interval i_dt
 
             // Time loop
-            while (ism == 0) { // Until mobility stationary state
+            while (!is_mobility_stat) { // Until mobility stationary state
 
                 mc_iter++;
                 i_count2++;
@@ -240,6 +251,7 @@ int main()
                         ic_b[in_x[i]][in_y[i]]++;
 
                     } // and then B particles
+                    
                 } // Sequential dynamics
 
                 // Update vectors and matrices
@@ -284,19 +296,113 @@ int main()
                 c += i_count2*phi_y;
                 d += i_count2*gini_a;
 
-                double di = static_cast<double>(idt*(idt+1))/2;
-                double di2 = static_cast<double>((2*idt+1)*di)/3;
+                double di = static_cast<double>(i_dt*(i_dt+1))/2;
+                double di2 = static_cast<double>((2*i_dt+1)*di)/3;
 
                 // Check if the system has reached stationary state
-                if (mc_iter == i_count*idt) {
+                if (mc_iter == i_count*i_dt) {
+                    // Calculate time average over i_dt
+                    double tavg_mobility = d_mom_mobility/i_dt;
+                    double tavg_phi_x = d_mom_phi_x/i_dt;
+                    double tavg_phi_y = d_mom_phi_y/i_dt;
+                    double tavg_j_a = d_mom_j_a/i_dt;
+                    double tavg_j_b = d_mom_j_b/i_dt;
+                    double tavg_gini_a =  d_mom_gini_a/i_dt;
+                    double tavg_gini_b = d_mom_gini_b/i_dt;
 
+                    // Calculate slopes
+                    double slope_mobility =  (i_dt*a - di*d_mom_mobility)/(i_dt*di2 - std::pow(di, 2));
+                    double slope_phi_x = (i_dt*b - di*d_mom_phi_x)/(i_dt*di2 - std::pow(di, 2));
+                    double slope_phi_y = (i_dt*c - di*d_mom_phi_y)/(i_dt*di2 - std::pow(di, 2));
+                    double slope_gini_a = (i_dt*d - di*d_mom_gini_a)/(i_dt*di2 - std::pow(di, 2));
+
+                    // Set counters
+                    i_count++;
+                    i_count2 = 0;
+
+                    // Store booleans
+                    is_mobility_stat = (std::abs(slope_mobility) < epsilon);
+                    is_phi_x_stat = (std::abs(slope_phi_x) < epsilon);
+                    is_phi_y_stat = (std::abs(slope_phi_y) < epsilon);
+                    is_gini_a_stat = (std::abs(slope_gini_a) < epsilon);
+
+                    // Calculate probabilities for each behaviour
+                    if (tavg_phi_x >= (1.0-erro)) p_lane += 1.0;
+                    else if (tavg_mobility <= erro) p_clog += 1.0;
+                    else p_else += 1.0;
+
+                    // Calculate sample average of stationary variables
+                    savg_mobility += tavg_mobility/n_run;
+                    savg_phi_x += tavg_phi_x/n_run;
+                    savg_phi_y += tavg_phi_y/n_run;
+                    savg_j_a += tavg_j_a/n_run;
+                    savg_j_b += tavg_j_b/n_run;
+                    savg_j = savg_j_a - savg_j_b;
+                    savg_gini_a += tavg_gini_a/n_run;
+                    savg_gini_b += tavg_gini_b/n_run;
+
+                    // Calculate sample second moment of stationary variables
+                    svar_mobility += std::pow(tavg_mobility, 2);
+                    svar_phi_x += std::pow(tavg_phi_x, 2);
+                    svar_phi_y += std::pow(tavg_phi_y, 2);
+                    svar_j_a += std::pow(tavg_j_a, 2);
+                    svar_j_b += std::pow(tavg_j_b, 2);
+                    svar_j += std::pow(tavg_j_a - tavg_j_b, 2);
+                    svar_gini_a += std::pow(tavg_gini_a, 2);
+                    svar_gini_b += std::pow(tavg_gini_b, 2);
+                    
                 }
+
+                if (mc_iter > 1e7) break;
 
             } // Time loop
 
         } // Sample loop
 
+        // Calculate sample variance
+        svar_mobility = svar_mobility/n_run - std::pow(savg_mobility, 2);
+        svar_phi_x = svar_phi_x/n_run - std::pow(savg_phi_x, 2);
+        svar_phi_y = svar_phi_y/n_run - std::pow(savg_phi_y, 2);
+        svar_j_a = svar_j_a/n_run - std::pow(savg_j_a, 2);
+        svar_j_b = svar_j_b/n_run - std::pow(savg_j_b, 2);
+        svar_j = svar_j/n_run - std::pow(savg_j, 2);
+        svar_gini_a = svar_gini_a/n_run - std::pow(savg_gini_a, 2);
+        svar_gini_a = svar_gini_a/n_run - std::pow(savg_gini_a, 2);
+        p_lane /= n_run;
+        p_clog /= n_run;
+        p_else /= n_run;
+        double var_p_lane = p_lane*(1 - p_lane);
+        double var_p_clog = p_clog*(1 - p_clog);
+        double var_p_else = p_else*(1 - p_else);
+
+        // Output data to file
+        data_file1  << rho_total << '\t' << rho_a << '\t' << rho_b << '\t' << savg_mobility << '\t'
+                    << savg_phi_x << '\t' << savg_phi_y << '\t'
+                    << savg_j << '\t' << savg_j_a << '\t' << savg_j_b << '\t'
+                    << savg_gini_a << '\t' << savg_gini_b << '\t'
+                    << p_lane << '\t' << p_clog << '\t' << p_else << '\n';
+
+        data_file2  << rho_total << '\t' << rho_a << '\t' << rho_b << '\t' << svar_mobility << '\t'
+                    << svar_phi_x << '\t' << svar_phi_y << '\t'
+                    << svar_j << '\t' << svar_j_a << '\t' << svar_j_b << '\t'
+                    << svar_gini_a << '\t' << svar_gini_b << '\t'
+                    << var_p_lane << '\t' << var_p_clog << '\t' << var_p_else << '\n';
+
+        // Increment particle number (max of 256 particles)
+        int d_n = static_cast<int>(l_x*l_y/256.0);
+        if (d_n < 1) d_n = 1;
+        n_a += d_n;
+        n_b += d_n;
+        n_total = n_a + n_b;
+
+        // Update number of samples and density
+        n_run = static_cast<int>(1000.0/n_total) + 200;
+        rho_a =  static_cast<double>(n_a)/(l_x*l_y);
+        rho_b =  static_cast<double>(n_b)/(l_x*l_y);
+        rho_total =  static_cast<double>(n_a+n_b)/(l_x*l_y);
+
     } // External loop
 
     return 0;
+    
 }
